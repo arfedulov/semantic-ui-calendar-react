92abd5f62ba28061a60d25089c73cd9e0b0b730a
Artem Fedulov <arfedulov@gmail.com>
2018-05-29 02:34:31 +0300

    fix: popup doesn't close on document click

--- src/containers/CustomPopup.js
+++ src/containers/CustomPopup.js
@@ -1,16 +1,47 @@
+import React from 'react';
 import { Popup } from 'semantic-ui-react';
+import keyboardKey from 'keyboard-key';
 
-class CustomPopup extends Popup {
+class CustomPopup extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      popupIsOpen: false
+    };
   }
 
-  getPortalProps = () => {
-    const portalProps = Popup.prototype.getPortalProps.call(this, arguments);
-    if (portalProps.closeOnDocumentClick) {
-      portalProps.closeOnDocumentClick = false;
+  handleOpen = () => {
+    this.setState({popupIsOpen: true});
+    setTimeout(() => {
+      this.popupCoords = document.getElementById('suirCalendarPopup').getBoundingClientRect();
+    }, 0);
+  }
+
+  handleClose = (e) => {
+    const clickCoords = { x: e.clientX, y: e.clientY};
+    if (this.shouldClosePopup(this.popupCoords, clickCoords) || keyboardKey.getCode(e) === keyboardKey.Escape) {
+      this.setState({ popupIsOpen: false});
     }
-    return portalProps;
+  }
+
+  shouldClosePopup = (popupCoords, clickCoords) => {
+    const { x, y, width, height } = popupCoords;
+    return clickCoords.x < x ||
+           clickCoords.y < y ||
+           clickCoords.x > x + width ||
+           clickCoords.y > y + height;
+  }
+
+  render() {
+    return (
+      <Popup
+        { ...this.props }
+        id="suirCalendarPopup"
+        onOpen={this.handleOpen}
+        onClose={this.handleClose}
+        open={this.state.popupIsOpen} />
+    );
   }
 }
 
